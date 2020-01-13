33d740976fc76fbda3684a9094a7d9fbd20c84be
kumquatPeels <jikster712@gmail.com>
2020-01-13 11:52:16 -0800

    view all products - Back End: error log modified PART II

--- server/index.js
+++ server/index.js
@@ -34,7 +34,6 @@ app.get('/api/products', (req, res, next) => {
       res.status(200).json(result.rows);
     })
     .catch(error => {
-      console.error(error);
       next(error);
     });
 });
@@ -47,6 +46,7 @@ app.use((err, req, res, next) => {
   if (err instanceof ClientError) {
     res.status(err.status).json({ error: err.message });
   } else {
+    console.error(err);
     res.status(500).json({
       error: 'an unexpected error occurred'
     });
